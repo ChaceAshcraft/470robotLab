fbf5c75bff583467e73c4da9ecd559a357566db4
C Chace Ashcraft <ashcr6@tsunami.cs.byu.edu>
2017-10-05 16:23:50 -0600

    almost works!

--- mse430_head/controller/examples/auto_pf_brian.py
+++ mse430_head/controller/examples/auto_pf_brian.py
@@ -8,10 +8,10 @@ from potentialField import PotentialField
 def main(host='localhost', port=55555, goal="I0"):
     """
     This file is a copy of 'guide.py' in the examples, modified to have the
-    robot navigate from a starting position to a goal through a field of 
-    obstacles.  
+    robot navigate from a starting position to a goal through a field of
+    obstacles.
 
-    Note that we always expect arugments to the main function from the 
+    Note that we always expect arugments to the main function from the
     command line. In particular, the goal value should be the marker
     label that the robot is seeking.
 
@@ -53,6 +53,9 @@ def main(host='localhost', port=55555, goal="I0"):
     def calc_angle(x, y):
         return atan2(-y, x)  # Reversed for upside-down y
 
+    def calc_angle2(x, y):
+        return atan2(y, x)  
+
     # Sometimes, when taking differences between angles, you end up
     # with something out of usable range. This fixes that by
     # constraining x to be between +/- pi.
@@ -68,11 +71,12 @@ def main(host='localhost', port=55555, goal="I0"):
     # PID is given a list of errors where the last error is the current error, as well as three proportionality constants
     # PID returns a number
     def PID(errors, KP, KD, KI):
+        print("errors, ", errors)
         term1 = KP * errors[-1]
         term2 = 0
         term3 = 0
         if len(errors) > 1:
-            term2 = KD * derivative(errors[-2:-1])
+            term2 = KD * derivative(errors[-2:])
             term3 = KI * np.trapz(errors)
         return term1 + term2 + term3
 
@@ -99,7 +103,7 @@ def main(host='localhost', port=55555, goal="I0"):
     lost_count = 0
 
     '''
-    One way to do this is to get all of the markers at the beginning 
+    One way to do this is to get all of the markers at the beginning
     and assign a potential field to each. Then, during the while
     loop, we assume that the markers do not move and only do field
     computations on the set fields and the current robot postion.
@@ -107,33 +111,34 @@ def main(host='localhost', port=55555, goal="I0"):
     fields = {}
     goalField = None
     max_rob_speed = 20 #adjust as needed
-    attractor_strength = 0.35
+    attractor_strength = 0.75
     repulsor_strength = 0.35
     tangent_strength = 0.35
-    trans_err_list = np.zeros(5)
-    angle_err_list = np.zeros(5)
+    trans_err_list = np.array([0,0,0,0,0])
+    angle_err_list = np.array([0,0,0,0,0])
 
-    k_trans = [0.5, 0.5, 0.5]
-    k_angle = [0.5, 0.5, 0.5]
+    k_trans = [0.01, 0.00, 0.00]
+    k_angle = [0.5, 0.1, 0.01]
 
     markers = do('where others')
-    marker_radius =  distance(markers[markers.keys()[0]]['corners'][0], 
-                                        markers[markers.keys()[0]]['center']) 
+    marker_radius =  distance(markers[goal]['corners'][0],
+                                        markers[goal]['center'])
     for key in markers.keys():
-        if key == goal:
-            location = markers[key]['center']
-            location[1] *= -1  # adjust for y being down
-            goalField = PotentialField(location, 10000, marker_radius/2, attractor_strength, 
-                                                                        type='attracter')
-        else:
-            location = markers[key]['center']
-            location[1] *= -1  # adjust for y being down
-            if np.random.randint(10) < 1: # 1/10 probability of tangent field
-                fields[key] = PotentialField(location, 2*marker_radius, marker_radius, repulsor_strength, 
-                                                                        type='tangent')
+        if key != 'time':
+            if key == goal:
+                location = markers[key]['center']
+                location[1] *= -1  # adjust for y being down
+                goalField = PotentialField(location, marker_radius/2, 20000, attractor_strength,
+                                                                        field_type='attractor')
             else:
-                fields[key] = PotentialField(location, 2*marker_radius, marker_radius, tangent_strength, 
-                                                                        type='repulsor')
+                location = markers[key]['center']
+                location[1] *= -1  # adjust for y being down
+            #    if np.random.randint(10) < 1: # 1/10 probability of tangent field
+            #         fields[key] = PotentialField(location, marker_radius, 10*marker_radius, repulsor_strength,
+            #                                                            field_type='tangent')
+            #    else:
+            fields[key] = PotentialField(location, marker_radius, 10*marker_radius, tangent_strength,
+                                                                        field_type='repulsor')
 
     # Running loop
     try:
@@ -152,7 +157,7 @@ def main(host='localhost', port=55555, goal="I0"):
                 cur_robot_angle = calc_angle(*res['orientation'])
                 '''
                 #Not sure we need this for our implementation
-                
+
                 # Calculate an error in the angle, which gives a
                 # direction (sign) to turn and also an idea of what
                 # speed to go (the magnitude). Note that this is the
@@ -176,29 +181,43 @@ def main(host='localhost', port=55555, goal="I0"):
 
                     # accrue effect of potential fields on the robot
                 cur_rob_loc = np.array(res['center'])
+                cur_rob_loc[1] *= -1 # adjust for y being down
 
-                if distance(cur_rob_loc, goalField.location) < marker_radius:
+                print("distance", distance(cur_rob_loc, goalField.location))
+                print("robot loc, ", cur_rob_loc)
+                print("goal loc, ", markers['16']['center']) 
+                print("goalfield_loc, ", goalField.location)
+                if distance(cur_rob_loc, goalField.location) < 3*marker_radius:
                     you_did_it = True
+                    do('speed 0 0')
                 else:
 
-                    cur_rob_loc[1] *= -1 # adjust for y being down
-                    field_effect = np.array([0, 0])
-                    field_effect += goalField.effect(cur_rob_loc)
-                    for field in fields.items():
-                        field_effect += field.effect(cur_rob_loc)
+                    field_effect = np.array([0., 0.])
+                    field_effect += np.array(goalField.effect(cur_rob_loc))
+                    for field in fields.values():
+                        print("obstacle effect: ", field.effect(cur_rob_loc))
+                        field_effect += np.array(field.effect(cur_rob_loc))
+
+                    print("field_effect: ", field_effect)
 
                     cur_trans_err = distance(cur_rob_loc, cur_rob_loc + field_effect)
-                    cur_angle_err = cur_robot_angle - calc_angle(field_effect)
-                    trans_err_list = trans_err_list[:-1]
-                    angle_err_list = angle_err_list[:-1]
-                    np.append(trans_err_list, cur_trans_err)
-                    np.append(angle_err_list, cur_angle_err)
+                    cur_angle_err =  calc_angle2(*field_effect) - cur_robot_angle
+                    trans_err_list = trans_err_list[1:]
+                    angle_err_list = angle_err_list[1:]
+                    trans_err_list = np.append(trans_err_list, cur_trans_err)
+                    angle_err_list = np.append(angle_err_list, cur_angle_err)
 
                     drive = PID(trans_err_list, k_trans[0], k_trans[1], k_trans[2])
                     turn = PID(angle_err_list, k_angle[0], k_angle[1], k_angle[2])
 
-                    do('speed {} {}'.format(round(drive - turn), round(drive + turn)))
-                    # sleep(0.05)
+                    print('Current angle: {}, Angle error: {}' .format(cur_robot_angle, cur_angle_err))
+                    print('Current Trans: {}, Field Effect: {}, Trans error: {}' .format(cur_rob_loc, field_effect, cur_trans_err))
+                    print('PID drive: {}, PID turn: {}'.format(drive, turn))
+                    print('Instructions: {}, {}'.format(drive - turn, drive + turn))
+                    print("goalField radius: ", goalField.radius)
+
+                    do('speed {} {}'.format(int(round(drive - 7*turn)), int(round(drive + 7*turn))))
+                    sleep(0.01)
 
             else:
                 # Sometimes the camera fails to find the robot, and it
--- mse430_head/controller/examples/potentialField.py
+++ mse430_head/controller/examples/potentialField.py
@@ -3,9 +3,9 @@ import math
 
 class PotentialField:
 
-    def __init__(self, location, null_radius, spread, strength, field_type='repulsor'):
+    def __init__(self, location, radius, spread, strength, field_type='repulsor'):
         self.location = location
-        self.radius = null_radius
+        self.radius = radius
         self.spread = spread
         self.field_type = field_type
         self.field_strength = strength
@@ -18,6 +18,7 @@ class PotentialField:
         if self.field_type == 'repulsor':
             d = self.distance(location, self.location)
             if d > self.radius + self.spread:
+                print("outside field")
                 return [0, 0]
             theta = math.atan2(self.location[1] - location[1], self.location[0] - location[0]) + np.pi/180
             #  a constant to give them some angle
@@ -29,6 +30,7 @@ class PotentialField:
         elif self.field_type == 'tangent':
             d = self.distance(location, self.location)
             if d > self.radius + self.spread:
+                print("outside field")
                 return [0, 0]
             theta = math.atan2(self.location[1] - location[1], self.location[0] - location[0]) + np.pi/2*0.95
             #  the constant term on the end gives the tangent angle minus a little to push outward as well
@@ -40,11 +42,13 @@ class PotentialField:
         elif self.field_type == 'attractor':
             d = self.distance(location, self.location)
             if d > self.radius + self.spread:  # if it is outside of the potential field's effect
+                print("outside effect")
                 return [0, 0]
             theta = math.atan2(self.location[1] - location[1], self.location[0] - location[0]) + np.pi/180
             #  a constant to give them some angle
             if d > self.radius: # within the radius of effect, but outside the object's own radius
-                return [self.field_strength*(self.spread + self.radius - d)*math.cos(theta),
-                        self.field_strength * (self.spread + self.radius - d) * math.sin(theta)]
+                return [self.field_strength*(d + 2)*math.cos(theta),
+                        self.field_strength * (d + 2) * math.sin(theta)]
             else: # 'On top' of the object 
+                print("on top")
                 return [0, 0]
