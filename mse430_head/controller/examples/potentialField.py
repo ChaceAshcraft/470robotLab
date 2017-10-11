169ddfd1cc36dda92dc2c9b42e829464f009416f
Chace <cc.ash.math@gmail.com>
2017-10-11 12:50:59 -0600

    added original implementation of potential fields to the file

--- mse430_head/controller/examples/potentialField.py
+++ mse430_head/controller/examples/potentialField.py
@@ -60,3 +60,42 @@ class PotentialField:
             else: # 'On top' of the object
                 return [self.field_strength*(d + 2)*math.cos(theta),
                         self.field_strength * (d + 2) * math.sin(theta)]
+    def o_effect(self, location):
+        if self.type == 'repulsor':
+            d = self.distance(location, self.location)
+            if d > self.radius + self.spread:
+                return [0, 0]
+            theta = math.atan2(self.location[1] - location[1], self.location[0] - location[0])
+            #  a constant to give them some angle
+            if d >= self.radius:
+                return [-self.field_strength*(self.spread + self.radius - d)*math.cos(theta),
+                        -self.field_strength * (self.spread + self.radius - d) * math.sin(theta)]
+            else:
+                return [-np.sign(math.cos(theta))*self.infinity, -np.sign(math.sin(theta))*self.infinity]
+        elif self.type == 'tangent':
+            d = self.distance(location, self.location)
+            if self.orient == 'counter':
+                theta = math.atan2(self.location[1] - location[1], self.location[0] - location[0]) + np.pi/4
+            elif self.orient == 'clock':
+                theta = math.atan2(self.location[1] - location[1], self.location[0] - location[0]) - np.pi/4
+            else:
+                raise ValueError("No correct orientation!")
+            if d > self.radius + self.spread:
+                return [0, 0]
+            #  the constant term on the end gives the tangent angle minus a little to push outward as well
+            if d > self.radius + 1:
+                return [-self.field_strength * (self.spread + self.radius - d) * math.cos(theta),
+                        -self.field_strength * (self.spread + self.radius - d) * math.sin(theta)]
+            else:
+                return [-np.sign(math.cos(theta)) * self.infinity, -np.sign(math.sin(theta)) * self.infinity] 
+        elif self.field_type == 'attractor':
+            d = self.distance(location, self.location)
+            theta = math.atan2(self.location[1] - location[1], self.location[0] - location[0])
+            if d > self.radius + self.spread:  
+                return [self.field_strength * self.spread * math.cos(theta), 
+                        self.field_strength * self.spread * math.sin(theta)]
+            if d >= self.radius: 
+                return [self.field_strength * (d - self.radius) * math.cos(theta),
+                        self.const_strength * (d - self.radius) * math.sin(theta)]
+            else: # 'On top' of the object
+                return [0, 0]
