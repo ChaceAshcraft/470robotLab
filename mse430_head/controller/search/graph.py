caff6eeae2ea6454deff329cefa6ce7c8aa65fb4
Chace <cc.ash.math@gmail.com>
2017-10-17 16:54:49 -0600

    implemented A*, hopefully correctly

--- mse430_head/controller/search/graph.py
+++ mse430_head/controller/search/graph.py
@@ -1,8 +1,9 @@
 import numpy as np
+import queue
 from node import search_node
 
-def distance(point1, point2):
-    return np.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+def mh_distance(point1, point2):
+    return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
 
 class Robot_AStar_Graph:
     """
@@ -30,7 +31,7 @@ class Robot_AStar_Graph:
         else:
             # Assign the heuristic function to be the distance between the node
             # location and the goal location
-            self.H = lambda x: distance(x.location, self.goal_location)
+            self.H = lambda x: mh_distance(x.location, self.goal_location)
 
     def search(self):
         """
@@ -40,4 +41,27 @@ class Robot_AStar_Graph:
         returns: list of nodes with the 0 index being the root and the -1 index
                 being the goal node.
         """
-        pass
+        goalNode = None
+        if root.hasGoal:
+            return [root.center]
+        PQ = queue.PriorityQueue()
+        root.isVisited = True
+        root.searchPathLength = 0
+        PQ.put((0, root))
+        while not PQ.empty() and goalNode is None:
+            (d,n) = PQ.get() # PQ.get() returns a tuple (priority, node)
+            for neighbor in n.neighbors:
+                if not neighbor.isVisited:
+                    neighbor.searchParent = n
+                    neighbor.searchPathLength = (n.searchPathLength + 
+                                        mh_distance(n.location, neighbor.location))
+                    if neighbor.hasGoal:
+                        goalNode = neighbor
+                        break
+                    else:
+                        PQ.put((neighbor.searchPathLength + self.H(neighbor), neighbor))
+
+        if goalNode is not None:
+            return goalNode.expandPath()
+        else:
+            return None
--- mse430_head/controller/search/node.py
+++ mse430_head/controller/search/node.py
@@ -1,4 +1,3 @@
-import numpy as np
 
 
 class search_Node:
@@ -9,7 +8,23 @@ class search_Node:
     """
 
     def __init__(self, location, neighbors=None, obstacle=False, goal=False):
+        """
+        param: location -- 2d vector denoting the top left corner of the node tile
+        param: neighbors -- list of nodes this node is connected to
+        param: obstacle -- True is there is an obstacle in this node, False otherwise
+        param: goal -- True is the goal is in in this node, False otherwise
+        
+        """
         self.location = location
         self.neighbors = neighbors
         self.hasObstacle = obstacle 
         self.hasGoal = goal 
+        self.isVisited = False
+        self.searchParent = None
+        self.searchPathLength = 0
+
+    def expandPath(self):
+        if self.searchParent is None:
+            return [self.center]
+        else:
+            return self.searchParent.expandPath().append(self.center)
